0e4f650093a546c8cc6fc5a5d12d8c9da6ec8ffc
kahleeeb3 <powellcalebm@gmail.com>
2025-02-19 12:02:45 -0500

    text clears after 3 seconds

--- main.py
+++ main.py
@@ -11,9 +11,20 @@ class DrawingApp:
         
         self.canvas.bind("<B1-Motion>", self.draw)
         
+        self.reset_timer()
+        
+    def reset_timer(self):
+        if hasattr(self, 'timer'):
+            self.root.after_cancel(self.timer)
+        self.timer = self.root.after(3000, self.clear_canvas)
+        
     def draw(self, event):
+        self.reset_timer()
         x, y = event.x, event.y
         self.canvas.create_oval(x-2, y-2, x+2, y+2, fill="black", outline="black")
+        
+    def clear_canvas(self):
+        self.canvas.delete("all")
 
 if __name__ == "__main__":
     root = tk.Tk()
